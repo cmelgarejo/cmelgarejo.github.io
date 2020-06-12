ff70a6fbbf3a92ab5ad799a20dc791f851819c5b
Christian Melgarejo Bresanovich <cmelgarejo@users.noreply.github.com>
2020-06-11 22:44:24 -0400

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -48,4 +48,4 @@ document.querySelectorAll('nav ul li').forEach(el =>
 }));
 
 document.getElementById('age').innerHTML = (new Date(new Date() - new Date('1981-01-01')).getFullYear() - 1970);;
-document.getElementById('agsenioritye').innerHTML = (new Date(new Date() - new Date('2003-01-01')).getFullYear() - 1970);;
+document.getElementById('seniority').innerHTML = (new Date(new Date() - new Date('2003-01-01')).getFullYear() - 1970);;
