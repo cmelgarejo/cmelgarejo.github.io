fbf6389cf609991143c6ddf9bc5f8aeb8cb1335c
cmelgarejo <cmelgarejo.dev@gmail.com>
2022-09-12 16:23:42 -0400

    Improvements
    Signed-off-by: cmelgarejo <cmelgarejo.dev@gmail.com>

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,7 +9,7 @@ const scrollAndResizeHandler = (e) => {
     document.querySelectorAll('nav ul li').forEach(e => (e.classList.remove('selected')));
     return;
   }
-  if (document.body.scrollTop > window.innerHeight * 0.95 || document.documentElement.scrollTop > window.innerHeight * 0.95) {
+  if (document.body.scrollTop > window.innerHeight * 0.32 || document.documentElement.scrollTop > window.innerHeight * 0.32) {
     document.getElementById('navbar').classList.add('shown');
   }
   else {
@@ -19,13 +19,12 @@ const scrollAndResizeHandler = (e) => {
   lastWindowWidth = document.documentElement.clientWidth;
 }
 const setSelectedNavLink = () => {
-  let sections = ['community', 'writing', 'projects', 'education', 'experience', 'introduction'];
+  let sections = ['writing', 'projects', 'education', 'experience', 'introduction'];
   let sectionScrolls = sections.map(v => ({
     name: v,
-    visible: document.getElementById(v).offsetTop <= (document.documentElement.scrollTop + 0.333 * document.documentElement.clientHeight)
+    // visible: document.getElementById(v).offsetTop <= (document.documentElement.scrollTop)
   }));
   sectionScrolls.sort((a, b) => b.visible - a.visible);
-  console.log(JSON.stringify(sectionScrolls));
   document.querySelectorAll('nav ul li').forEach(e => (e.classList.remove('selected')));
   document.getElementById(`${sectionScrolls[0].name}Link`).classList.add('selected');
 }
@@ -33,10 +32,6 @@ document.addEventListener("DOMContentLoaded", function() {
   document.getElementById('seniority').innerHTML = (new Date(new Date() - new Date('2005-01-01')).getFullYear() - 1970);
   document.addEventListener('scroll', scrollAndResizeHandler);
   window.addEventListener('resize', scrollAndResizeHandler);
-  document.getElementsByClassName('nav-icon')[0].addEventListener('click', e => {
-    document.getElementsByClassName('nav-icon')[0].classList.toggle('open');
-    document.getElementById('navbar').classList.toggle('shown');
-  });
   document.querySelectorAll('nav ul li').forEach(el =>
     el.addEventListener('click', function(e){
     if(document.documentElement.clientWidth < 560) {
